d18c700c16814dce52b3ca97cd584058fb98fd81
Mira Elbasha <Mira2malak@gmail.com>
2026-04-09 17:36:12 +0200

    Add project files.

---
+++ AssignmentLINQ02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+namespace AssignmentLINQ02
+{
+    class Program
+    {
+        static void Main()
+        {
+           
+        }
+
+      
+    }
+}
